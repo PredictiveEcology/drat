10a4adf464d8daa66a75dc16d3fab589655f1a51
Alex Chubaty <achubaty@for-cast.ca>
2021-02-25 20:09:07 -0700

    add win binaries

---
+++ build_binaries.R
@@ -0,0 +1,17 @@
+if (interactive()) {
+  unlink(c("packages", "packages_src"), recursive = TRUE)
+
+  drat.builder::build(install = TRUE, no_commit = TRUE)
+}
+
+### ---------------------------------------------------- ###
+
+pkgs <- drat.builder:::read_packages()
+
+binPkgs <- apply(pkgs, 1, function(p) {
+  pkg <- p["path_pkg"]
+  devtools::install_dev_deps(pkg, dependencies = TRUE) # shouldn't be necessary using drat.builder(install = TRUE)
+  devtools::build(pkg = pkg, path = "packages", binary = TRUE)
+})
+
+drat::insertPackages(binPkgs, repodir = ".", action = "archive")
