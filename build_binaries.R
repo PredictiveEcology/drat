b8aed4c9a08b4c57ece0407b0276dbcd479613eb
Alex Chubaty <achubaty@for-cast.ca>
2021-03-04 10:32:32 -0700

    manually re-add windows binaries

--- build_binaries.R
+++ build_binaries.R
@@ -12,10 +12,10 @@ pkgs <- drat.builder:::read_packages()
 binPkgs <- apply(pkgs, 1, function(p) {
   pkg <- p["path_pkg"]
   devtools::install_dev_deps(pkg, dependencies = TRUE, upgrade = FALSE) # shouldn't be necessary using drat.builder(install = TRUE)
-  devtools::build(pkg = pkg, path = "packages", binary = TRUE)
+  devtools::build(pkg = pkg, path = "packages", binary = TRUE, args = "--no-multiarch")
 })
 
-drat::insertPackages(binPkgs, repodir = ".", action = "archive")
+drat::insertPackages(binPkgs, repodir = ".", action = "prune")
 
 if (interactive()) {
   drat::pruneRepo(repopath = ".", type = "source", remove = "git")
